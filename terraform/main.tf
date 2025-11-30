698476c2d9ddbcd0a601f041b1c673b703d7d858
sfbarragan845 <stevenfernando-22@hotmail.com>
2025-11-30 16:37:33 -0500

    feat(terraform): add DNS prefix for Azure Kubernetes cluster Introduces a DNS prefix configuration for the Azure Kubernetes cluster to facilitate easier access and management, improving the clarity of the service's endpoint.
    Also aids in better deployment organization and environment consistency.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  dns_prefix          = "flask-aks"
 
   default_node_pool {
     name       = "default"
