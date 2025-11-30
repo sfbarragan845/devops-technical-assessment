c3a92d7098480917a243c4ddf1a434e146c13dbc
sfbarragan845 <stevenfernando-22@hotmail.com>
2025-11-30 16:12:14 -0500

    feat(terraform): add variables for Azure resource configuration
    Introduces essential Terraform variables for managing Azure resources, including resource group name, location, AKS cluster name, ACR name,
    Docker image, and node count.
    
    Also facilitates easier customization and deployment of infrastructure for the DevOps technical assessment by parameterizing key resource
    settings.

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type    = string
+  default = "rg-devops-aks"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "aks_name" {
+  type    = string
+  default = "aks-devops"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "devopstechnicalassessment"
+}
+
+variable "docker_image" {
+  type    = string
+  default = "devopstechnicalassessment.azurecr.io/devops-technical-assessment:latest"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
