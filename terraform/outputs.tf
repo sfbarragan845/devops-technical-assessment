cda707b6bc0ac4e83f60aabeb6c829c4afb107d8
sfbarragan845 <stevenfernando-22@hotmail.com>
2025-11-30 16:23:58 -0500

    feat(terraform): add output definitions for AKS cluster and Flask service Adds output variables to expose the AKS cluster name and the IP address of the Flask service, facilitating easier access to important deployment information.
    Also enables smoother integration with automation scripts and monitoring
    tools.

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "flask_service_ip" {
+  value = kubernetes_service.flask.status[0].load_balancer[0].ingress[0].ip
+}
