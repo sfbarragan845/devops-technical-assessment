493649bf857267c0ad3d6f32721adace89210e31
sfbarragan845 <stevenfernando-22@hotmail.com>
2025-11-30 16:46:02 -0500

    fix(terraform): update Flask service output to show production IP Modifies the output to retrieve the public IP of the Flask production service instead of the previous version, improving accuracy.
    Also adds a description for clarity on what the output represents, enhancing readability and usability.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,6 @@ output "aks_cluster_name" {
 }
 
 output "flask_service_ip" {
-  value = kubernetes_service.flask.status[0].load_balancer[0].ingress[0].ip
+  value = kubernetes_service.flask_production.status[0].load_balancer[0].ingress[0].ip
+  description = "Public IP of the Flask production service"
 }
